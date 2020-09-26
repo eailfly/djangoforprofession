9669373aaa104576082ca3850c08347503f3d5a5
EailFly Bai <eailfly@gmail.com>
2020-09-26 17:39:20 +0800

    ch6

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -120,9 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/tests.py
+++ users/tests.py
@@ -46,7 +46,7 @@ class SignupPageTests(TestCase):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'signup.html')
         self.assertContains(self.response, 'Sign Up')
-        self.assertNotContains(self.response, 'Log In')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
 
     def test_signup_form(self) -> None:
         form = self.response.context.get('form')
