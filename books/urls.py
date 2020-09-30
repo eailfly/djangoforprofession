5f1aebc385a12622ccda57218993791929e4906c
EailFly Bai <eailfly@gmail.com>
2020-09-30 14:14:06 +0800

    ch15

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookListView, BookDetialView
+from .views import BookDetialView, BookListView, SearchResultsListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetialView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results')
 ]
